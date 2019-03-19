ef89b754b0f090a3152360c794c3472bf762b6d4
Nicola Halim <staticddq@gmail.com>
2019-03-19 12:35:49 +1100

    Player now moves using mouse right click

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float rotSpeed = 2f;
     [SerializeField] private Image healthBar;
     private Rigidbody rb;
-    private float health = 100;
+    private int health = 100;
+
+    private Vector3 targetPos;
+    private Vector3 lookAtTarget;
+    private Quaternion playerRot;
+    private bool isMoving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,17 +23,45 @@ public class PlayerControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float moveHori = Input.GetAxis("Horizontal") * moveSpeed;
-        float moveVert = Input.GetAxis("Vertical") * moveSpeed;
-
-        Vector3 movement = new Vector3(moveHori,0, moveVert);
-
-        rb.AddForce(movement);
+        if (Input.GetMouseButtonDown(1))
+        {
+            SetTargetPosition();
+        }
+        if(isMoving)
+            MovePlayer();
     }
 
-    public void TakeDamage(float dmg)
+    private void SetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 1000))
+        {
+            targetPos = hit.point;
+            targetPos.y = transform.position.y;
+            lookAtTarget = new Vector3(targetPos.x - transform.position.x, transform.position.y, targetPos.z - transform.position.z);
+            playerRot = Quaternion.LookRotation(lookAtTarget);
+            playerRot.x = 0;
+            playerRot.z = 0;
+            isMoving = true;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, rotSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
+
+        if((transform.position - targetPos).magnitude < 2f)
+        {
+            isMoving = false;
+        }
+    }
+
+    public void TakeDamage(int dmg)
     {
         health -= dmg;
         healthBar.fillAmount = health / 100f;
